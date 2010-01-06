dffc76f8fe46a25ef4c8e372aa46e967e7af613c
Mark Percival <mark@mpercival.com>
2010-01-06 16:26:32 -0500

    rocking the Hoptoad the right way

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
 #SquarePush Hoptoad account
 HoptoadNotifier.configure do |config|
-  config.api_key = 'df6812b9d8c6a9f9107debf7e6cd8017a6a3458a'
+  config.api_key = '363bbae01c3301df2a90b5a43d2b9da8'
 end
