7b791c89eaa4baf5f56a4b7f546087a25af023c2
Mark Percival <mark@mpercival.com>
2010-02-08 11:40:39 -0500

    FUCK Gem Bundler. No seriosly, fuck it in the ass. I've never been so fucking fed up with some piece of shit. Fuck Carl and fuck Yehuda. You can't just fucking change the API every week, along with making it actually worse by the day. FFFFFFFFFUUUUUUUUUUUUUUU

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Be sure to restart your server when you modify this file
-
+begin
+  # Require the preresolved locked set of gems.
+  require File.expand_path('../../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
@@ -21,6 +29,21 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  config.gem "rails", :version => "2.3.5"
+  config.gem "haml"
+  config.gem "oauth"
+  config.gem "oauth"
+  config.gem "authlogic"
+  config.gem "nokogiri"
+  config.gem "four_bites_aes"
+  config.gem "nokogiri"
+  config.gem "mysql"
+  config.gem "fakeweb"
+  config.gem "will_paginate"
+  config.gem "factory_girl"
+  config.gem "rspec", :version => "1.2.6"
+  config.gem "rspec-rails"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
