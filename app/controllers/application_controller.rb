974dcf80ab49fabd32a3d23fa83c0c68e1796496
Mark Percival <mpercival@twitter.com>
2010-08-18 08:13:49 -0700

    i19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,50 +6,67 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   helper_method :current_user_session, :current_user, :title
 
-  private
-    def self.title(new_title = nil)
-      if new_title
-        @title = new_title
-      else
-        @title
-      end
-    end
-    
-    def title
-      @title || self.class.title
-    end
-  
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
+  before_filter :set_locale
 
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
+  def set_locale
+    # if params[:locale] is nil then I18n.default_locale will be used
+    if params[:locale]
+      I18n.locale = params[:locale]
+    else
+      I18n.locale = extract_locale_from_accept_language_header
     end
-    
-    def redirect_back_or_default(default)  
-      redirect_to(session[:return_to] || default)  
-      session[:return_to] = nil  
+  end
+
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
+  def self.title(new_title = nil)
+    if new_title
+      @title = new_title
+    else
+      @title
     end
-    
-    def require_no_user
-      if current_user
-        redirect_to account_url
-      end
+  end
+
+  def title
+    @title || self.class.title
+  end
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  def require_no_user
+    if current_user
+      redirect_to account_url
     end
-    
-    def require_user
-      unless current_user
-        redirect_to login_url
-      end
+  end
+
+  def require_user
+    unless current_user
+      redirect_to login_url
     end
-    
-    def require_admin
-      unless current_user && current_user.admin
-        redirect_to login_url
-      end
+  end
+
+  def require_admin
+    unless current_user && current_user.admin
+      redirect_to login_url
     end
+  end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 SHORT_DOMAIN = '2D4.ME' unless defined? SHORT_URL
 
@@ -38,5 +38,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  
+
 end
